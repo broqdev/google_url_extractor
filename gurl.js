89577e714aa3a4226a1a6e9040bdc5b39d5ef273
0rchard <0rchard.git@gmail.com>
2012-08-15 22:47:03 +0800

    ver 0.1

---
+++ gurl.js
@@ -0,0 +1,78 @@
+function gurlClick(info, tab) {
+    if (info.menuItemId == "gurlCopy") {
+        var url = gurlGetOrigin(info.linkUrl);
+        console.log(url);
+        gurlClip(url);
+        return;
+    }
+
+//    if (info.menuItemId == "gurlOpen") {
+//        var url = gurlGetOrigin(info.linkUrl);
+//        console.log(url);
+//        chrome.tabs.create({"url":url}, null);
+//        return;
+//    }
+}
+
+function gurlParser(url) {
+    var a = document.createElement('a');
+    a.href = url;
+    return a;
+}
+
+function gurlIsGoogle(url) {
+    var a = gurlParser(url);
+    console.log(a.hostname);
+    var ret = false;
+    if (a.hostname.search("google") >= 0 )
+        ret = true;
+
+    return ret;
+}
+
+function gurlGetOrigin(url) {
+    var a = gurlParser(url);
+    var seg = a.search.replace(/^\?/,'').split('&');
+    for (var i=0;i<seg.length;i++) {
+        if (!seg[i])
+            continue;
+
+        s = seg[i].split('='); 
+        if (s[0] == "url")
+            return decodeURIComponent(s[1]);
+    }
+    return null;
+}
+
+function gurlClip(url) {
+    if (url == null)
+        return;
+
+    var t = document.createElement("textarea");
+    var bodys = document.getElementsByTagName("body");
+    if (bodys == null || 0 >= bodys.length)
+        return;
+
+    var body = bodys[0];
+    body.appendChild(t);
+    t.value = url;
+    t.select();
+    document.execCommand("copy");
+    body.removeChild(t);
+}
+
+function gurlShow(details) {
+    var contexts = ["link"];
+    var menus = [
+    { "id":"gurlCopy", "title": "copy original url", "contexts": contexts, "onclick": gurlClick },
+    { "id":"gurlOpen", "title": "open original url", "contexts": contexts, "onclick": gurlClick }
+    ];
+    var url = details.url;
+    chrome.contextMenus.removeAll(null);
+    if (gurlIsGoogle(url)) {
+        chrome.contextMenus.create(menus[0], null);
+        //chrome.contextMenus.create(menus[1], null);
+    }
+}
+
+chrome.webNavigation.onCommitted.addListener(gurlShow);
